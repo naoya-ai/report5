799fe07a7a3865b4cd813da5329aeb8505183b99
naoya-ai <konakurisatsuma23@icloud.com>
2024-12-12 19:42:33 +0900

    step2

---
+++ src/report5.java
@@ -0,0 +1,12 @@
+public class report5{
+    public static void main(String[] args) {
+        try{
+            String str = null;
+            System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました！");
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
